33a36ceb9b2ec3906907b1d86512e7d159fa8c58
Mauricio <mauhmg@gmail.com>
2023-04-24 11:38:15 -0300

    Alter status bar style

--- App.tsx
+++ App.tsx
@@ -3,11 +3,11 @@ import { StatusBar, StyleSheet, View } from 'react-native';
 import { Home } from './src/screens/Home';
 
 export default function App() {
-  
+
 	return (
 		<View style={styles.app}>
 			<StatusBar
-				barStyle="light-content"
+				barStyle="dark-content"
 				translucent
 			/>
 			<Home />
